73859af2a8c96686d62234fea176000ea7bd72c5
JHorrigan <jahorrigan1411@gmail.com>
2020-11-13 17:33:37 +0000

    Add Custom Pause Button Functionality in JS

--- js/video.js
+++ js/video.js
@@ -4,10 +4,17 @@ window.onload = function() {
 
     // Get the buttons
     var playBtn = document.getElementById("playBtn");
+    var pauseBtn = document.getElementById("pauseBtn");
 
     // Add an event listener for the play button
     playBtn.addEventListener("click", function(e) {
         // Play the video
         video.play();
+    });
+
+    // Add an event listener for the pause button
+    pauseBtn.addEventListener("click", function(e) {
+        // Pause the video
+        video.pause();
     });
 }
