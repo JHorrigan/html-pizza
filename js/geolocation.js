4a31847eb902fae3ca54ee1921805632a1ee27fb
JHorrigan <jahorrigan1411@gmail.com>
2020-11-21 14:20:48 +0000

    Call findNearest Function

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,8 +2,8 @@ window.onload = function() {
     // Check to see if the user's browser supports GeoLocation
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            alert("Latitude: " + position.coords.latitude + " " +
-                  "Longitude: " + position.coords.longitude);
+            // Pass the location data to the findNearest method
+            findNearest(position.coords.latitude, position.coords.longitude);
         });
     }
 }
