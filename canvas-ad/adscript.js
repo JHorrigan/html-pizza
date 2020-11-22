34a7088b15d6365fb2ce263982f690120c6637d7
JHorrigan <jahorrigan1411@gmail.com>
2020-11-22 10:41:03 +0000

    Check for Canvas Support & Initialise 2D Drawing Context

--- canvas-ad/adscript.js
+++ canvas-ad/adscript.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-    
+    var adCanvas = document.getElementById("adCanvas");
+    if (adCanvas.getContext) {
+        // Initialise a 2d drawing context
+        var ctx = adCanvas.getContext("2d");
+    }
 }
